90f23d380867f4fb445bb4ba04e8cb889ae922e8
v1ctoriale1gh <victoria@victorialeigh.tech>
2021-02-14 17:31:18 -0500

    Updates

---
+++ precache-manifest.4a5a82d863d05310d57a3f92f4a6fc1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceee5151b6bcfabd0fc29952b051f21c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8c6a3c1510f68d742908",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "24f38a3886e48ece1356",
+    "url": "/portfolio/static/css/main.80d0a4ca.chunk.css"
+  },
+  {
+    "revision": "8c6a3c1510f68d742908",
+    "url": "/portfolio/static/js/2.cc6a45ad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.cc6a45ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f38a3886e48ece1356",
+    "url": "/portfolio/static/js/main.4c3a4d3d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
